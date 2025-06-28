512833d4a29c35a0545f2170d9ea77cf0eceb89f
YahyaMohamed3 <yahya11212006@gmail.com>
2025-06-28 04:45:00 -0400

    unesscarsy files

--- try.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyCv5xRq0RrBd_Jc3KdmW-vXNdJtxkI8gTk")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
